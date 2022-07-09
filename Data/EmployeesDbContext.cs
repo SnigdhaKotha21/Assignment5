0578198ae501cc4696fd364e5ab1b2493f3da5da
SnigdhaKotha21 <kotha.snigdha12@gmail.com>
2022-07-09 15:27:15 +0530

    second commit

---
+++ Data/EmployeesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment5.Models;
+
+namespace Assignment13.Data
+{
+    public class EmployeesDbContext : DbContext
+    {
+        public EmployeesDbContext (DbContextOptions<EmployeesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment5.Models.Employee> Employee { get; set; }
+    }
+}
